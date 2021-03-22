c87266079249cfc00e2b207f79f3a5dea2cd1270
Trikita73 <diachenkoandrii7@gmail.com>
2021-03-23 01:41:35 +0200

    3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,34 @@
 function getMinMax(str) {
-  // ваш код...
+	let CommaSplit = str.split(',');
+	//console.log(CommaSplit);
+
+	let SpaceMap = CommaSplit.map((item) => item.split(' '));
+ 	//console.log(SpaceMap);
+
+ 	let numbers =[];
+
+ 	SpaceMap.forEach((item) => { 
+ 		item.forEach((itemElement) => {
+
+	 		if(itemElement === '') {
+	 			return;
+	 		}
+
+	 		let num = parseFloat(itemElement, 10);
+
+	 		
+	 		if(isNaN(num)) {
+	 			return;
+	 		}
+	 		
+	 		numbers.push(num);
+ 		})
+ 	});	
+
+ 	const min = Math.min(numbers);
+ 	const max = Math.max(numbers);
+
+ 	return {
+ 		min, max
+ 	}
 }
