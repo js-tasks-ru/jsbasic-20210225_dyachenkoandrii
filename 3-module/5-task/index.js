5349f7ea8e1314b76be2effa6d00eb2d887b499f
Trikita73 <diachenkoandrii7@gmail.com>
2021-04-01 11:33:05 +0300

    3-M 5-tusk

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,11 +2,7 @@ function getMinMax(str) {
 
 	let SpaceMap = str.split(' ').join(',').split(',');
 
-
  	let arr = SpaceMap.filter((item) => isFinite(item));
 
- 	min = Math.min(...arr);
- 	max = Math.max(...arr);
-
- 	return {min, max}
+ 	return { min: Math.min(...arr), max: Math.max(...arr) }
 }
