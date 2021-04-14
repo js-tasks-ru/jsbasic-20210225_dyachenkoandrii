3e19f128dc58602f4b01bb7a14bb4f7c089f517b
Trikita73 <diachenkoandrii7@gmail.com>
2021-04-15 01:59:07 +0300

    4-module 1/3 -task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,15 +1,9 @@
- function makeFriendsList (friends) {
+function makeFriendsList (friends) {
 
-  const ul = document.createElement('ul');
+ 	let ul = document.createElement('ul');	
+	let li = document.createElement('li');
 
-    friends.forEach((item) => {
-    const li = document.createElement('li');
-      li.appendChild(document.createTextNode(`${item.firstName}, ${item.lastName}`));
-      ul.appendChild(li);
-    });
-
-    document.body.appendChild(ul);
+	  ul.insertAdjacentHTML('beforeEnd', 'li');
 
   return ul;
 }
-
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
-function highlight(table) {
-  // ваш код...
+function highlight(table) { 
+
+	let body = table.getElementsByTagName('tbody');
+	let tr = body.getElementsByTagName('tr');
+
+	let  array = Array.from(tr);
+
+	array.forEach((row) => {
+
+	let fmaleList = row.getElementsByTagName('td');
+	
+        if(fmaleList.textContent === 'm') {
+            row.classList.add('male');
+        }
+
+        else if(fmaleList.textContent === 'f') {
+            row.classList.add('female');
+        }
+
+	});
+
 }
