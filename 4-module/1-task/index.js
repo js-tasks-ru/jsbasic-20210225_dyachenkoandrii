a0d50c75411a5372538a9b4d16b751f51a35b801
Trikita73 <diachenkoandrii7@gmail.com>
2021-04-19 02:05:59 +0300

    4-module 1/3-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,7 +3,11 @@ function makeFriendsList (friends) {
  	let ul = document.createElement('ul');	
 	let li = document.createElement('li');
 
-	  ul.insertAdjacentHTML('beforeEnd', 'li');
+	friends.forEach((item, arr) => {
+	
+		ul.insertAdjacentHTML('beforeEnd', '<li>'+item.firstName+item.lastName+'</li>');
 
+	});
+	  
   return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -7,15 +7,31 @@ function highlight(table) {
 
 	array.forEach((row) => {
 
-	let fmaleList = row.getElementsByTagName('td');
+	let statusList = row.getElementsByTagName('td');
+
+		if(statusList.dataset.available === 'true') {
+			row.classList.add('available');
+		} 
+
+		else if(statusList.dataset.available === 'false') {
+			row.classList.add('unavailable');
+		}
+
+	let genderList = row.getElementsByTagName('td');
 	
-        if(fmaleList.textContent === 'm') {
+        if(genderList.textContent === 'm') {
             row.classList.add('male');
         }
 
-        else if(fmaleList.textContent === 'f') {
+        else if(genderList.textContent === 'f') {
             row.classList.add('female');
         }
+
+    let decorationList = row.getElementsByTagName('td');
+    
+    	if(decorationList.textContent < 18) {
+    		row.style.textDecoration = 'line-through';
+    	}    
 
 	});
 
